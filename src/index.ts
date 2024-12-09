581b2022e43881f8f94b92e1641548ac6ea5900e
Ignacio <ignacioaroza.ia@gmail.com>
2024-12-09 10:39:09 -0300

    add cors settings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectDB from './infrastructure/database/mongoose';
 import urlRoutes from './http/routes/urlRoutes';
 import userRoutes from './http/routes/userRoutes'
@@ -10,6 +11,20 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173', 'https://url-shortener-hzvf.onrender.com/'];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
+  })
+)
 
 connectDB();
 
